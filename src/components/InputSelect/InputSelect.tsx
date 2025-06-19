e0060722ed97f37e0fb9b31af568422a6da242f5
Luuk <luuk@voorhoede.nl>
2025-06-19 10:09:34 +0200

    Add select styles
    Makes it look more like DatoCMS components

--- src/components/InputSelect/InputSelect.tsx
+++ src/components/InputSelect/InputSelect.tsx
@@ -3,6 +3,7 @@ import { forwardRef } from "react";
 import { FieldError, FieldHint } from "datocms-react-ui";
 
 import "./InputSelect.css";
+import ChevronDownIcon from "../ChevronDownIcon/ChevronDownIcon";
 
 type Props = {
   itemLength: number;
@@ -14,7 +15,23 @@ export default forwardRef<any, Props>(
   function InputSelect({ itemLength, min, max, ...props }, ref) {
     return (
       <>
-        <AsyncSelect className="input-select" isDisabled={itemLength === max} {...props} ref={ref} />
+        <AsyncSelect
+        {...props}
+        className="input-select"
+        classNames={{
+          control: () => "input-select__control",
+          indicatorSeparator: () => "input-select__indicator-separator",
+        }}
+        components={{
+          DropdownIndicator: () => (
+            <div className="input-select__dropdown-indicator">
+              <ChevronDownIcon/>
+            </div>
+          )
+        }}
+        placeholder=""
+        isDisabled={itemLength === max}
+        ref={ref} />
         { (min && itemLength < min) && <FieldError>You need to select at least {min} items. Select { min - itemLength } more item(s).</FieldError> }
         { max && <FieldHint>You have selected {itemLength} / {max} items.</FieldHint> }
       </>
