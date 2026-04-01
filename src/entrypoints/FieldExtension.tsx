1c7892acc6a1f07794448c9506b9685fd5c18e79
Luuk <luuk@voorhoede.nl>
2026-04-01 12:11:33 +0200

    Remove unused lodash and React imports
    Remove debounce from loadOptions and useEffect dependencies

--- src/entrypoints/FieldExtension.tsx
+++ src/entrypoints/FieldExtension.tsx
@@ -1,8 +1,8 @@
-import { debounce, get } from "lodash";
+import { get } from "lodash";
 import { RenderFieldExtensionCtx } from "datocms-plugin-sdk";
 import { Canvas } from "datocms-react-ui";
 import type { Parameters } from "../types/parameters";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import getDataFromPath from "../utils/getDataFromPath";
 import parseString from "../utils/parseString";
 import formatData from "../utils/formatData";
@@ -25,40 +25,44 @@ export default function FieldExtension({ ctx }: Props) {
   );
 
   function loadOptions(inputValue: string) {
-    return new Promise<SelectOptionType[]>(async (resolve, reject) => {
-      try {
-        const url = new URL(parseString(parameters.searchUrl, { query: inputValue }));
-        const proxy = new URL('https://cors-proxy.datocms.com');
-        proxy.searchParams.set('url', url.href);
+    return new Promise<SelectOptionType[]>((resolve, reject) => {
+      const url = new URL(
+        parseString(parameters.searchUrl, { query: inputValue }),
+      );
+      const proxy = new URL("https://cors-proxy.datocms.com");
+      proxy.searchParams.set("url", url.href);
 
-        const headers = {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        };
+      const headers = {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      };
 
-        if (parameters.additionalHeaders) {
-          Object.assign(headers, JSON.parse(parameters.additionalHeaders));
-        }
-
-        const response = await fetch(parameters.useCORSProxy ? proxy : url, {
-          method: "GET",
-          headers
-        });
-        const data = await response.json();
-        const dataFromPath = getDataFromPath(data, parameters.path);
-        const formattedData = formatData(dataFromPath, parameters);
-        resolve(
-          formattedData
-            .map((item: any) => ({
-              value: item.id,
-              label: item.title,
-              data: { ...item },
-            }))
-            .filter((item: any) => !value.find((i: { id: string }) => i.id === item.data.id)),
-        );
-      } catch (error) {
-        reject(error);
+      if (parameters.additionalHeaders) {
+        Object.assign(headers, JSON.parse(parameters.additionalHeaders));
       }
+
+      fetch(parameters.useCORSProxy ? proxy : url, {
+        method: "GET",
+        headers,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const dataFromPath = getDataFromPath(data, parameters.path);
+          const formattedData = formatData(dataFromPath, parameters);
+          resolve(
+            formattedData
+              .map((item: any) => ({
+                value: item.id,
+                label: item.title,
+                data: { ...item },
+              }))
+              .filter(
+                (item: any) =>
+                  !value.find((i: { id: string }) => i.id === item.data.id),
+              ),
+          );
+        })
+        .catch(reject);
     });
   }
 
@@ -73,7 +77,9 @@ export default function FieldExtension({ ctx }: Props) {
 
   useEffect(() => {
     /* fieldValue contains all sorts of characters like \n which will never be matched, so we have to parse and stringify to get rid of them */
-    const oldFieldValue = JSON.stringify(JSON.parse(get(ctx.formValues, ctx.fieldPath) as string));
+    const oldFieldValue = JSON.stringify(
+      JSON.parse(get(ctx.formValues, ctx.fieldPath) as string),
+    );
     const newFieldValue = JSON.stringify(value);
 
     if (oldFieldValue !== newFieldValue) {
@@ -81,18 +87,13 @@ export default function FieldExtension({ ctx }: Props) {
     }
   }, [value]);
 
-  const debouncedLoadOptions = useMemo(
-    () => debounce(loadOptions, 300),
-    [loadOptions]
-  );
-
   return (
     <Canvas ctx={ctx}>
       <InputSelect
         itemLength={value.length}
         min={parameters.min ? Number(parameters.min) : undefined}
         max={parameters.max ? Number(parameters.max) : undefined}
-        loadOptions={debouncedLoadOptions}
+        loadOptions={loadOptions}
         onChange={(item) => {
           selectItem(item as SelectOptionType);
           setSelectValue(null);
@@ -103,7 +104,12 @@ export default function FieldExtension({ ctx }: Props) {
       <output>
         <SelectedList items={value} handleDragEnd={setValue}>
           {value.map((item) => (
-            <SelectedListItem key={item.id} id={item.id} item={item} removeItem={removeItem}/>
+            <SelectedListItem
+              key={item.id}
+              id={item.id}
+              item={item}
+              removeItem={removeItem}
+            />
           ))}
         </SelectedList>
       </output>
